e7a5c51d5f8a3a556626e1439fd2ffb8f16ec94a
Trace White <traceannwhite@gmail.com>
2021-11-30 17:20:03 -0600

    added tech info and scrollbar

--- components/Skills.js
+++ components/Skills.js
@@ -3,11 +3,11 @@ import Link from "next/link";
 export default function Skills() {
   return (
     <div>
-      <p>
+      <p className="skills">
         HTML | CSS | Sass | JavaScript | Python | jQuery | React.js | Redux |
         Next.js | ReactStrap | Node | Git | Github | Chrome Dev Tools | MongoDB
         | MongoDB Realm | Django | Mongoose | Postman | Oauth | Heroku | Vercel
-        | WordPress
+        | WordPress | PHP | Laravel | Shopify | Webflow
       </p>
     </div>
   );
