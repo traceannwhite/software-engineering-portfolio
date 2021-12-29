06a892d62af2c0e9c8581c0e75e965ef54b48302
Trace White <traceannwhite@gmail.com>
2021-12-29 10:18:13 -0600

    line height global change

--- components/ProjectCard.js
+++ components/ProjectCard.js
@@ -5,25 +5,23 @@ const ProjectCard = ({ project, index }) => {
   return (
     <div key={index} className={styles.recentwork}>
       <Link href={`/projects/${project.slug}`}>
-        <div className={styles.overlay}>
-          <div className={styles.container}>
-            <div
-              className={styles.display}
-              style={{
-                backgroundImage: `url(
+        <div className={styles.container}>
+          <div
+            className={styles.display}
+            style={{
+              backgroundImage: `url(
               "https://res.cloudinary.com/dhcagrzcb/image/upload/v1640635230/macbook-white_d4g6vs.png"
             )`,
-              }}
-            >
-              <div
-                className={styles.image}
-                style={{ backgroundImage: `url(${project.image})` }}
-              ></div>
-            </div>
-            <div className={styles.details}>
-              <h2 className={styles.name}>{project.name}</h2>
-              <p className={styles.short}>{project.short}</p>
-            </div>
+            }}
+          >
+            <div
+              className={styles.image}
+              style={{ backgroundImage: `url(${project.image})` }}
+            ></div>
+          </div>
+          <div className={styles.details}>
+            <h2 className={styles.name}>{project.name}</h2>
+            <p className={styles.short}>{project.short}</p>
           </div>
         </div>
       </Link>
