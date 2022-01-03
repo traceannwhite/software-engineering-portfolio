4db0d8a01f7e7b97cc45c19f0c7b0a7c7172a753
Trace White <traceannwhite@gmail.com>
2022-01-02 21:32:23 -0600

    small change

--- components/Introduction.js
+++ components/Introduction.js
@@ -4,7 +4,7 @@ import BioPhoto from "./BioPhoto";
 const Introduction = () => {
   return (
     <div className={styles.container}>
-      <h1 className={styles.intro}>Hi, I&apos;m Trace</h1>
+      <h2 className={styles.intro}>Hi, I'm Trace</h2>
       <BioPhoto />
       <div className={styles.box}>
         <ul className={styles.list}>
@@ -20,3 +20,75 @@ const Introduction = () => {
 };
 
 export default Introduction;
+
+{
+  /* <span className={styles.textMotion} style={{ transitionDelay: "0s" }}>
+          H
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.02s" }}
+        >
+          i
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.04s" }}
+        >
+          ,
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.06s" }}
+        >
+          {" "}
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.08s" }}
+        >
+          I
+        </span>
+        <span className={styles.textMotion} style={{ transitionDelay: "0.1s" }}>
+          &apos;
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.12s" }}
+        >
+          m
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.14s" }}
+        >
+          {" "}
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.16s" }}
+        >
+          T
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.18s" }}
+        >
+          r
+        </span>
+        <span className={styles.textMotion} style={{ transitionDelay: "0.2s" }}>
+          a
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.22s" }}
+        >
+          c
+        </span>
+        <span
+          className={styles.textMotion}
+          style={{ transitionDelay: "0.24s" }}
+        >
+          e
+        </span> */
+}
